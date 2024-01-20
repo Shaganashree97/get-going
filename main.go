99496581b0c19b2d37a55de5faa8857f5d49e377
Shaganashree P L <120777273+Shaganashree97@users.noreply.github.com>
2024-01-20 06:31:26 +0000

    Print, Println

---
+++ main.go
@@ -0,0 +1,20 @@
+package main  // says it belongs to 'main' package
+
+import "fmt"  // importing fmt package, means format, it provides functions for formatting input and output
+
+func main() {   // no semicolon or newline should be present after main function
+    fmt.Println("Hello!")  // Println function from the "fmt" package to print
+	fmt.Print("Shaganashree")  // Println provides new line after the print is executed
+	fmt.Println(" P L")
+}
+
+/*
+Output
+
+
+/*
+func main() 
+{}  
+
+This type of spacing will cause error
+*/
